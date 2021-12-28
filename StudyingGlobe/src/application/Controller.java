08909f6548179de9781cea805e6498d4debd6390
doowoncho <84430881+doowoncho@users.noreply.github.com>
2021-12-28 17:57:53 -0500

    timer
    cant stop it though

--- StudyingGlobe/src/application/Controller.java
+++ StudyingGlobe/src/application/Controller.java
@@ -1,25 +1,90 @@
 package application;
 
+import java.awt.Event;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class Controller {
 
 	
 	
 		@FXML
+		
 		private TextField text;
 	
 		@FXML
 		private Button button;
 		
+		@FXML
+		Button btwn;
+		
+		private Scene scene;
+		private Parent root;
+		private Stage stage;
+		
+		public boolean start = false;
+		boolean test;
+		@FXML
+		public void pressed(ActionEvent event) throws Exception
+		{
+			Date currentDate = new Date();
+			SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
+			System.out.println(timeFormat.format(currentDate));
+			
+			Parent root = FXMLLoader.load(getClass().getResource("scene2.fxml"));
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+			
+			start = true;
+			timer();
+	
+					
+		}
 		
 		@FXML
-		void pressed(ActionEvent event)
+		public void end(ActionEvent event) throws Exception
 		{
-			System.out.println("pressed!");
+			start = false;
+			Date currentDate = new Date();
+			SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
+			System.out.println(timeFormat.format(currentDate));
+			
+			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		
+			
+		}
+		
+		
+		public void timer() throws InterruptedException
+		{
+			int x = 0;
+			
+			
+			while(start)
+			{
+				
+				Thread.sleep(1000);
+				System.out.println(x);
+				x++;				
+			}
+			
+			return;
 		}
 		
 } 
